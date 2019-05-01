20cf3810926d47da324e165c6779cc068e050d02
Jon Daniel <binarycleric@gmail.com>
2019-04-30 21:43:51 -0400

    We can force a bustin

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,22 @@ extern crate chrono;
 use std::collections::HashMap;
 use rocket_contrib::templates::Template;
 use chrono::prelude::*;
+use std::env;
+
 
 #[get("/")]
 fn index() -> Template {
     let mut context = HashMap::new();
     let dt = Local::now();
+    let force_bustin = env::var("FORCE_BUSTIN").unwrap_or("0".to_string()) == "1";
 
-    context.insert("thursday", dt.weekday() == Weekday::Thu);
+    if force_bustin || dt.weekday() == Weekday::Thu {
+        context.insert("bustin", true);
+    } else {
+        context.insert("bustin", false);
+    }
+    context.insert("forced", force_bustin);
+
     Template::render("index", &context)
 }
 
