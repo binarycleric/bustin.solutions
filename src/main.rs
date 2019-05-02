1e2c9cda8bba94b1a669c63df851bdc00beb69fc
Jon Daniel <binarycleric@gmail.com>
2019-05-01 22:02:05 -0400

    Using eastern time

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@
 #[macro_use] extern crate rocket;
 extern crate rocket_contrib;
 extern crate chrono;
+extern crate chrono_tz;
 
 use std::collections::HashMap;
 use rocket_contrib::templates::Template;
 use chrono::prelude::*;
-use std::env;
+use chrono_tz::America::New_York;
 
+use std::env;
 
 #[get("/")]
 fn index() -> Template {
     let mut context = HashMap::new();
-    let dt = Local::now();
+    let dt = Local::now().with_timezone(&New_York);
     let force_bustin = env::var("FORCE_BUSTIN").unwrap_or("0".to_string()) == "1";
     let bustin_time = force_bustin || dt.weekday() == Weekday::Thu;
 
